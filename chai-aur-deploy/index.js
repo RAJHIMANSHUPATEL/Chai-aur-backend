2ce0bc9182a18a1ce3d8d38671e52667110e0a16
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-09-19 01:51:55 +0530

    chai-aur-deploy

---
+++ chai-aur-deploy/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+
+app.get('/', (req,res)=> {
+    res.send("Hello")
+})
+
+app.get('/twitter', (req,res)=> {
+    res.send("himanshurajpatel")
+})
+
+app.get("/login", (req,res)=> {
+    res.send('<h1>Himanshu Raj Patel</h1>')
+})
+
+app.listen(process.env.PORT, ()=> {
+    console.log("Server is listening on port 3000")
+})
